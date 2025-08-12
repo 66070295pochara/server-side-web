e710a9a80a9a94866a7141d70e009aa9bde24c03
66070295pochara <139258047+66070295pochara@users.noreply.github.com>
2025-08-12 14:07:52 +0700

    Add files via upload

---
+++ week4/index.js
@@ -0,0 +1,18 @@
+// index.js
+const express = require('express');
+require('./config/db'); 
+const productRoutes = require('./routes/productRoutes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./config/swagger');
+
+const app = express();
+app.use(express.json());
+
+// Swagger UI
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Routes
+app.use('/api', productRoutes); // <-- แก้ตรงนี้f
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
