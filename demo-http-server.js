bbb1b534838b8bcfa53f0aa26d834153a3a4528e
= <66070295@kmitl.ac.th>
2025-08-24 14:46:54 +0700

    add /about

--- demo-http-server.js
+++ demo-http-server.js
@@ -1,16 +1,21 @@
 // demo-http-server.js
-const http = require('http');           // เรียกใช้โมดูล http ของ Node.js
-const hostname = 'localhost';           // ชื่อโฮสต์ (127.0.0.1)
-const port = 3000;                      // กำหนดพอร์ตให้ server
+const http = require('http');
+const hostname = 'localhost';
+const port = 3000;
 
-// สร้าง server
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;                             // สถานะการตอบกลับ (200 = OK)
-  res.setHeader('Content-Type', 'text/plain');      // กำหนดชนิดของ response = text
-  res.end('Hello, World!\n');                       // ส่งข้อความกลับไปหา client
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/about') {
+    res.end('About Page\n');
+  } else if (req.url === '/contact') {
+    res.end('Contact Page\n');
+  } else {
+    res.end('Hello, World!\n');
+  }
 });
 
-// สั่งให้ server เริ่มฟัง request ที่ hostname + port
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
