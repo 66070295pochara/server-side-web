1666d70618f9416a59371d556883ccaaa4a81393
66070295pochara <139258047+66070295pochara@users.noreply.github.com>
2025-09-18 14:53:57 +0700

    Add files via upload

---
+++ lab10/models/User.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import mongoose from 'mongoose'
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  phone: {
+    type: String
+  },
+  role:{
+    type: String,
+    default: "user",
+
+  }
+})
+
+const User = mongoose.model("User", UserSchema);
+
+export default User
